b9a1d46de316144bf6c229002f22dfaa593c9309
zer0es <74467527+onesorzer0es@users.noreply.github.com>
2023-01-31 16:55:30 +0700

    Revert "Add func1 and func2"

--- main.py
+++ main.py
@@ -3,16 +3,8 @@
 def func1():
 	print("Hello World!")
 
-def func2():
-	print("How are you feel today?")
-
-def func3():
-	print("In God We Trust!")
-
 def main():
 	func1()
-	func2()
-	func3()
 
 if __name__ == "__main__":
     main()
